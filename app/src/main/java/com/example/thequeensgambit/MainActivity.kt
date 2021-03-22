8ee7ab67851095b2bed64786f602a5277d6a9f9a
Khanh Duy <kduy410@gmail.com>
2021-03-22 16:14:25 +0700

    Fixed crash caused by printWriter not initialize when don't use socket

--- app/src/main/java/com/example/thequeensgambit/MainActivity.kt
+++ app/src/main/java/com/example/thequeensgambit/MainActivity.kt
@@ -8,6 +8,11 @@ import com.example.thequeensgambit.chess.ChessBoard
 import com.example.thequeensgambit.chess.ChessDelegate
 import com.example.thequeensgambit.chess.ChessModel
 import com.example.thequeensgambit.chess.ChessPiece
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.*
+import java.util.concurrent.Executors
 
 const val TAG = "MainActivity"
 
@@ -15,8 +20,11 @@ const val TAG = "MainActivity"
  * Model - View - Controller
  */
 class MainActivity : AppCompatActivity(), ChessDelegate {
-    var chessModel = ChessModel()
-    lateinit var chessView: ChessBoard
+    private var chessModel = ChessModel()
+    private lateinit var chessView: ChessBoard
+    private var printWriter: PrintWriter? = null
+    private val PORT: Int = 50001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +37,36 @@ class MainActivity : AppCompatActivity(), ChessDelegate {
             chessModel.reset()
             chessView.invalidate()
         }
+        /**
+         * On Device
+         * Settings | Connections(WI-FI) | (active network) | IP address(192.168.232.2)
+         *
+         * auth vPaivCKgUP4BXE0R
+         * redir add tcp:50000:50001
+         */
+
+        findViewById<Button>(R.id.btn_connect).setOnClickListener {
+            Log.d(TAG, "Socket  client connecting to port:[$PORT]...")
+
+            Executors.newSingleThreadExecutor().execute {
+                try {
+                    val socket = Socket("192.168.232.2", PORT) // 172.16.2.4
+                    receiveMove(socket)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    throw e
+                }
+            }
+
+        }
+        findViewById<Button>(R.id.btn_listen).setOnClickListener {
+            Log.d(TAG, "Socket  client listening to port:[$PORT]...")
+            Executors.newSingleThreadExecutor().execute {
+                val serverSocket = ServerSocket(PORT)
+                val socket = serverSocket.accept()
+                receiveMove(socket)
+            }
+        }
     }
 
     override fun pieceAt(col: Int, row: Int): ChessPiece? {
@@ -36,8 +74,29 @@ class MainActivity : AppCompatActivity(), ChessDelegate {
     }
 
     override fun movePieceAt(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int) {
-
         chessModel.movePieceAt(fromCol, fromRow, toCol, toRow)
-        findViewById<ChessBoard>(R.id.chess_board)?.invalidate()
+        chessView.invalidate()
+        printWriter?.let {
+            val moveStr = "$fromCol,$fromRow,$toCol,$toRow"
+            Log.d(TAG, moveStr)
+            Executors.newSingleThreadExecutor().execute {
+                it.println(moveStr)
+            }
+        }
+    }
+
+    override fun receiveMove(socket: Socket?) {
+        socket?.let {
+            val scanner = Scanner(socket.getInputStream())
+            printWriter = PrintWriter(socket.getOutputStream(), true)
+            while (scanner.hasNextLine()) {
+                val move: List<Int> = scanner.nextLine().split(",").map {
+                    it.toInt()
+                }
+                runOnUiThread {
+                    movePieceAt(move[0], move[1], move[2], move[3]) // L - T - R - B
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/thequeensgambit/chess/ChessDelegate.kt
+++ app/src/main/java/com/example/thequeensgambit/chess/ChessDelegate.kt
@@ -1,6 +1,9 @@
 package com.example.thequeensgambit.chess
 
+import java.net.Socket
+
 interface ChessDelegate {
     fun pieceAt(col: Int, row: Int): ChessPiece?
     fun movePieceAt(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int)
+    fun receiveMove(socket: Socket?)
 }
